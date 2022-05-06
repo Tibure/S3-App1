cf3fa5edbfa751f4a7a129b9c2037efffd562411
EmileBureau <emilebureau@hotmail.com>
2022-05-06 11:18:15 -0400

    Ajout du factory pour les types d'ingrédients; manque a mettre les ingrédients abstract pour utiliser le code

---
+++ code/ingredients/IngredientFactory.java
@@ -0,0 +1,22 @@
+package ingredients;
+
+import ingredients.exceptions.IngredientException;
+
+public class IngredientFactory {
+    public Ingredient getIngredient(TypeIngredient aType) throws IngredientException {
+        switch (aType){
+            case FRUIT:
+                return new Fruit();
+            case LAITIER:
+                return new Laitier();
+            case VIANDE:
+                return new Viande();
+            case LEGUME:
+                return new Legume();
+            case EPICE:
+                return new Epice();
+            default:
+                throw new IngredientException("Type d'ingrédient invalide.");
+        }
+    }
+}
